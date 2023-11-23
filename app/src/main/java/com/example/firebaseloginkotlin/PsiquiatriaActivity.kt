6165cdbcb298a762179ceccd620d15fc1ccd5a27
JesusEduardoMar <112047568+JesusEduardoMar@users.noreply.github.com>
2023-11-22 21:48:22 -0600

    Casi al 100%

--- app/src/main/java/com/example/firebaseloginkotlin/PsiquiatriaActivity.kt
+++ app/src/main/java/com/example/firebaseloginkotlin/PsiquiatriaActivity.kt
@@ -1,43 +1,83 @@
 package com.example.firebaseloginkotlin
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class PsiquiatriaActivity : AppCompatActivity() {
+
+    // Lista de doctores, con enlaces predefinidos
+    val listaDoctores = mutableListOf<Doctor>(
+        Doctor("Dr. Juan Pérez", true, "abc123"),
+        Doctor("Dra. María González", false, "xyz456"),
+    )
+
+    private lateinit var database: FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_psiquiatria)
+        setContentView(R.layout.activity_psiquiatra)
 
-        val buttonDoctor1: Button = findViewById(R.id.buttonDoctor1)
-        val buttonDoctor2: Button = findViewById(R.id.buttonDoctor2)
-        val buttonDoctor3: Button = findViewById(R.id.buttonDoctor3)
+        // Inicializar Firebase
+        database = FirebaseDatabase.getInstance()
 
-        // Asigna un listener al botón de agendar cita del primer doctor
-        buttonDoctor1.setOnClickListener {
-            // Código para abrir la actividad de agendar cita y pasar los datos necesarios
-            val intent = Intent(this@PsiquiatriaActivity, AgendarCitaActivity::class.java)
-            intent.putExtra("doctorName", "Dr. Juan Carlos") // Ejemplo de dato a pasar (nombre del doctor)
-            // Agrega más datos necesarios según tus requerimientos
-            startActivity(intent)
+        val btnAgendarCita: Button = findViewById(R.id.btnAgendarCita)
+        val etNombrePaciente: EditText = findViewById(R.id.etNombrePaciente)
+        val etEmail: EditText = findViewById(R.id.etEmail)
+
+        btnAgendarCita.setOnClickListener {
+            val doctorSeleccionado = seleccionarDoctor()
+
+            if (doctorSeleccionado != null && esHoraDeConsulta() && doctorSeleccionado.disponible) {
+                val urlConsulta = generarURLConsulta(doctorSeleccionado, etNombrePaciente.text.toString())
+                mostrarMensaje("Tu URL de consulta: $urlConsulta")
+
+                // Agendar la cita en Firebase Realtime Database
+                agendarCita(doctorSeleccionado, etNombrePaciente.text.toString(), etEmail.text.toString())
+            } else {
+                mostrarMensaje("No se puede agendar la cita. Por favor, verifica el horario y la disponibilidad del doctor.")
+            }
         }
-        buttonDoctor2.setOnClickListener {
-            // Código para abrir la actividad de agendar cita y pasar los datos necesarios
-            val intent = Intent(this@PsiquiatriaActivity, AgendarCitaActivity::class.java)
-            intent.putExtra("doctorName", "Dr. Juan Carlos") // Ejemplo de dato a pasar (nombre del doctor)
-            // Agrega más datos necesarios según tus requerimientos
-            startActivity(intent)
-        }
-        buttonDoctor3.setOnClickListener {
-            // Código para abrir la actividad de agendar cita y pasar los datos necesarios
-            val intent = Intent(this@PsiquiatriaActivity, AgendarCitaActivity::class.java)
-            intent.putExtra("doctorName", "Dr. Juan Carlos") // Ejemplo de dato a pasar (nombre del doctor)
-            // Agrega más datos necesarios según tus requerimientos
-            startActivity(intent)
-        }
+    }
+
+    private fun seleccionarDoctor(): Doctor? {
+        // Puedes personalizar la lógica para seleccionar al doctor según tus necesidades
+        return listaDoctores.firstOrNull()
+    }
+
+    private fun esHoraDeConsulta(): Boolean {
+        val horaActual = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        return horaActual in 9..25
+    }
+
+    private fun generarURLConsulta(doctor: Doctor, nombrePaciente: String): String {
+        // Construir el enlace completo con el prefijo "https://meet.google.com/"
+        return "https://meet.google.com/${doctor.enlaceReunion}?name=$nombrePaciente"
+    }
+
+    private fun agendarCita(doctor: Doctor, nombrePaciente: String, email: String) {
+        // Generar un ID único para la cita
+        val citaId = UUID.randomUUID().toString()
+
+        // Crear el objeto Cita
+        val cita = Cita(citaId, nombrePaciente, email /*, Agrega más campos según tus necesidades */)
+
+        // Guardar la cita en Firebase Realtime Database
+        val citasRef = database.getReference("citas")
+        citasRef.child(citaId).setValue(cita)
+
+        // Mostrar mensaje de éxito o cualquier otra acción que desees realizar
+        mostrarMensaje("Cita agendada con éxito. La doctora será notificada.")
+    }
+
+    private fun mostrarMensaje(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
     }
 }
 
-
+data class Doctor(val nombre: String, val disponible: Boolean, val enlaceReunion: String)
+data class Cita(val citaId: String, val nombrePaciente: String, val email: String /* Agrega más campos según tus necesidades */)
